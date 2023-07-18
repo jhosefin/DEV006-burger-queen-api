f7589fbec09b8421d5fbce363f9ab1101788a38a
jhosefin <90260920+jhosefin@users.noreply.github.com>
2023-07-18 14:44:38 -0400

    otros

--- connect.js
+++ connect.js
@@ -10,8 +10,7 @@ async function connect() {
   try {
     await client.connect();
     const db = client.db();
-    console.log('Conexión exitosa a la base de datos',db);
-    return db
+    return db;
     // Puedes realizar operaciones en la base de datos utilizando el objeto `db`
     // Por ejemplo: const collection = db.collection('myCollection');
   } catch (error) {
